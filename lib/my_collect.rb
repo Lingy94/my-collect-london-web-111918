69db4162708e4b1683b229d9cbe32ef38c5503a6
Jack Ling <j.lingyx@gmail.com>
2018-10-17 14:40:58 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
 i = 0
 newArray = []
 while i < collection.length
-    yield collection[i]
+    yield newArray.push(collection[i]
     newArray.push(collection[i])
    i += 1
   end
